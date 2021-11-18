79eab71de900bf507a4d27c36548cd1afddb60d6
Mohammedaliyassen <73065131+Mohammedaliyassen@users.noreply.github.com>
2021-11-18 20:28:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -82,8 +82,8 @@ const closeMenu = ()=>{
 
 const random = ['images/image-product-1.jpg','images/image-product-2.jpg',
 'images/image-product-3.jpg','images/image-product-4.jpg'] 
-const previousImg = '';
-const currentImg = 0;
+let previousImg = '';
+let currentImg = 0;
 
 const next = ()=>{
     if(currentImg < 3){
